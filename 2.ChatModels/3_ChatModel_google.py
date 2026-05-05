9513f9188f3c7fd23d8ec6b7d5daddf1e6524db0
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-05-05 23:42:54 +0530

    qf

--- 2.ChatModels/3_ChatModel_google.py
+++ 2.ChatModels/3_ChatModel_google.py
@@ -3,4 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model = )
+model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
+
+res = model.invoke('what is the capital of india')
+
+print(res.content)
